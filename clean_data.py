4ec3aa54da68fd16aa01658c451d5c732faf86a9
mahmoudbmoussa <74250916+MahMawd@users.noreply.github.com>
2024-04-25 01:43:02 +0100

    Add files via upload

--- clean_data.py
+++ clean_data.py
@@ -1,12 +1,15 @@
 import pandas as pd
 
 
-path = "Books_Data_Clean.xlsx"
-df = pd.read_excel(path)
+path_books="Books_Data_Clean.xlsx" #path books file
+path_games = "vgsales.xlsx" #path games file
 
-def CleanSpecialChars(column_name):
+
+def CleanSpecialChars(path,column_name):
+    df = pd.read_excel(path)
     clean_df = df[~df[column_name].str.contains(r'[^\x00-\x7F]', na=False)]
+    print(clean_df["Name"])
     clean_df.to_excel(path, index=False)
-
-# CleanSpecialChars("Book Names")
-CleanSpecialChars("Author")
+    
+#CleanSpecialChars(path_books,"Book Names") #delete special characters in books file
+#CleanSpecialChars(path_games,"Name") #delete special characters in vsg file
